a5fd076ee8ee1eba8c97dcf1b0119c7ec88f6502
Emerson <emersonbferreira157@gmail.com>
2026-01-30 15:31:16 -0300

    exercicio 2 de logica com js

---
+++ exerciciojantar.js
@@ -0,0 +1,28 @@
+// EXERCÍCIO 2 - Página 41 do livro de lógica de programação e algorítimos com JavaScript de Edécio Iepsen
+
+// Elaborar um programa que leia o valor de um jantar. Calcule e informe o valor da taxa do garçom (10%)
+// e o valor total a ser pago.
+// EXEMPLO: 
+//Valor do jantar: R$80,00
+// Taxa do garçom: R$8,00
+// Total a pagar: R$88,00
+
+let valorjantar = 80;
+let taxagarçom = 10;
+let total = 0;
+
+total = 80 + ((taxagarçom / 100) * 80);
+
+//Gambiarra
+//console.log ("Valor do jantar: R$" + valorjantar + ",00") 
+//Isso é meio gambiarra pois é mais complicado, o certo caso precise adicionar
+//algo no final da frase seria com o uso de ( `` )
+
+//Forma "correta"
+console.log(`Valor do jantar: R$${valorjantar},00`);
+
+//Porcentagem do garçom
+console.log(`Taxa do Garçom: R$${(taxagarçom / 100) * 80},00`);
+
+// O total a pagar
+console.log(`Total a Pagar: R$${total},00`)
