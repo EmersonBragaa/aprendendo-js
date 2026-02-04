70f0c31a355e80e0fe020e8aa9d68d39616dfdb2
Emerson <emersonbferreira157@gmail.com>
2026-02-04 14:22:26 -0300

    primeiroproblema-hackerrank

--- lojaepagamento.js
+++ lojaepagamento.js
@@ -1,14 +1,16 @@
 // EXERCÍCIO 3 - Página 45 do livro Lógica de programação e algoritimos com JS de Edécio Iepsen 
 
-// Elaborar um programa pra uma loja, o qual leia o preço de um produto e informe as opções de pagamento da loja. Calcule e informe o valor para pagamento á vista com 10% de desconto e o valor em 3x.
+// Elaborar um programa pra uma loja, o qual leia o preço de um produto e informe as opções de pagamento da loja.
+//  Calcule e informe o valor para pagamento á vista com 10% de desconto e o valor em 3x.
 //EXEMPLO:                                                                                                                    Preço: R$60,00 
 // À vista: R$54.00
 // Ou 3x de R$20,00
 
 
-//Não nomeia como produto cabeçudo do caralho, SOMENTE AS OPÇÕES DE PAGAMENTO!!!! coloca o preço cheio, o com desconto a vista que nesse caso é de 10% e o parcelado 
+//Não nomeia como produto cabeçudo do caralho, SOMENTE AS OPÇÕES DE PAGAMENTO!!!! 
+// coloca o preço cheio, o com desconto a vista que nesse caso é de 10% e o parcelado 
 let preco = 60;
-let aVista = 10;
+let aVista = 50;
 let totalAVista = 0;
 
 totalAVista = (preco - (aVista / 100) * preco);
