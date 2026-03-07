ccfd8a61863e0bec7eb75b2b571fdc6d350c3c15
Emerson Ferreira <emersonbferreira157@gmail.com>
2026-03-07 17:03:56 -0300

    exercicio do livro 'logica de programação e algoritmos com JS'

---
+++ pizzariaLPAJ.js
@@ -0,0 +1,7 @@
+let valorTotal = 150;
+let clientes = 3;
+let valorPorCliente = 0;
+
+   valorPorCliente = valorTotal / clientes;
+   console.log(`O valor total é de R$${valorTotal},00`);
+   console.log(`O valor para cada um é de R$${valorPorCliente},00`);
