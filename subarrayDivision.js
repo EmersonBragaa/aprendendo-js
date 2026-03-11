bed357eed826607b3b4a29110365bfde24fdaa4f
Emerson Ferreira <emersonbferreira157@gmail.com>
2026-03-11 18:05:18 -0300

    Exercício 'subarray division' do hackerRank

---
+++ subarrayDivision.js
@@ -0,0 +1,15 @@
+function birthday(s, d, m) {
+    let count = 0;
+    let sum = 0;
+      for(let index = 0; index <= (s.length - m); index += 1){
+        for(let index2 = 0; index2 < m; index2 += 1){
+            sum = sum + s[index + index2];
+        }
+        if(sum === d){
+            count += 1;
+        }
+        sum = 0;
+      }
+      return count;
+};
+birthday([2, 2, 1, 3, 2], 4, 2);
