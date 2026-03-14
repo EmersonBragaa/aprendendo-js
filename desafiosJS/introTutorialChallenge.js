6311c9aa231bd899814c412ee39640013700b69e
Emerson Ferreira <contato.emersondev@gmail.com>
2026-03-14 11:08:26 -0300

    Desafio 'Intro To Tutorial Challenge' do HackerRank

---
+++ desafiosJS/introTutorialChallenge.js
@@ -0,0 +1,10 @@
+function introTutorial(V, arr) {
+    for(let index = 0; index < arr.length; index += 1){
+        if(arr[index] === V){
+            return index;
+        }
+    }
+
+}
+
+console.log(introTutorial(4, [1, 4, 5, 7, 9, 12]));
