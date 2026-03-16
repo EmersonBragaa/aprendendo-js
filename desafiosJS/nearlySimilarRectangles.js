c43ddb01133a168ef4644cb3289234b154d9d314
Emerson Ferreira <contato.emersondev@gmail.com>
2026-03-16 10:01:05 -0300

    feat: add nearlySimilarRectangles logic using nested loops

---
+++ desafiosJS/nearlySimilarRectangles.js
@@ -0,0 +1,19 @@
+function nearlySimilarRectangles(sides){
+    let sum = 0;
+    let x, y = 0;
+    sides.map((side, index) => {
+        x = side[0];
+        y = side[1];
+        for(let index2 = index + 1; index2 < sides.length; index2 += 1){
+            if(x / sides[index2][0] === y / sides[index2][1]){
+                sum += 1;
+            }
+        }
+    })
+    return sum
+}
+
+console.log(nearlySimilarRectangles([
+    [4, 8],
+    [15, 30],
+    [25, 50]]));
