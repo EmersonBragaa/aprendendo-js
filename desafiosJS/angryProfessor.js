821becb0771e630b617b00ce8056b94c922809da
Emerson Ferreira <contato.emersondev@gmail.com>
2026-03-17 09:07:25 -0300

    feat: implement angryProfessor challenge logic

---
+++ desafiosJS/angryProfessor.js
@@ -0,0 +1,14 @@
+function angryProfessor(k, a) {
+    let onTime = 0;
+    for(let index = 0; index < a.length; index += 1){
+        if(a[index] <= 0){
+            onTime += 1;
+        }
+    }
+
+    return onTime >= k ? 'NO' : 'YES';
+}
+
+console.log(angryProfessor(3, [-2, -1, 0, 1, 2]));
+console.log(angryProfessor(3, [-1, -2, 4, 2]));
+console.log(angryProfessor(2, [0, -1, 2, 1]));
