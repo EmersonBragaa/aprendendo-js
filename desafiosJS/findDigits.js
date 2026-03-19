d6468c491a72ac38ab5aa715e1ea8bc1d43bb923
Emerson Ferreira <contato.emersondev@gmail.com>
2026-03-19 19:00:53 -0300

    feat: add findDigits challenge solution

---
+++ desafiosJS/findDigits.js
@@ -0,0 +1,11 @@
+function findDigits(n) {
+    let arr = n.toString().split('');
+    let count = 0;
+    for(let index = 0; index < arr.length; index += 1){
+        if(Number.isInteger(n / arr[index])){
+            count += 1;
+        }
+    }
+    return count;
+}
+console.log(findDigits(1012));
