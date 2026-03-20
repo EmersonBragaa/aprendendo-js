899796c58626eb2a07a0a57b1ea69152ea66abd9
Emerson Ferreira <contato.emersondev@gmail.com>
2026-03-20 17:09:20 -0300

    feat: add minimum distances challenge solution

---
+++ desafiosJS/minimunDistances.js
@@ -0,0 +1,17 @@
+function minimumDistances(a) {
+    let pairs = [];
+    for(let index = 0; index < a.length; index += 1){
+        for(let index2 = index + 1; index2 < a.length; index2 += 1){
+            if(a[index] === a[index2]){
+                pairs.push(index2 - index);
+            }
+        } 
+      } if(pairs.length != 0){
+            return pairs.sort((a, b) => a - b)[0];
+    } else{
+        return - 1;
+    }
+    
+}
+console.log(minimumDistances([7, 1, 3, 4, 1, 7]));
+console.log(minimumDistances([1, 2, 3, 4, 10]));
